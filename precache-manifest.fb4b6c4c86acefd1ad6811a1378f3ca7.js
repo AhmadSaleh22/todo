d226e52edd91b029949b98aa11d0a770e87c0760
AhmadSaleh22 <mm755067@gmail.com>
2020-07-23 16:15:13 +0300

    Updates

---
+++ precache-manifest.fb4b6c4c86acefd1ad6811a1378f3ca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbff74d7612d53b96a45eb8e3dfad83",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a9043ffefd8eb665ccc2",
+    "url": "/todo/static/css/main.a4cec662.chunk.css"
+  },
+  {
+    "revision": "391bf176187237855b95",
+    "url": "/todo/static/js/2.6ef725d7.chunk.js"
+  },
+  {
+    "revision": "052157e0581e32a2b8fa8f7f1414d257",
+    "url": "/todo/static/js/2.6ef725d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9043ffefd8eb665ccc2",
+    "url": "/todo/static/js/main.9632f36c.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
